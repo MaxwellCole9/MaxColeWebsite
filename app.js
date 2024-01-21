5446baa61f2774aec63878cd4dce5c775abcfdba
Max Cole <colemax@oregonstate.edu>
2024-01-20 19:39:49 -0800

    optimized filespace, minor fixes

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const fs = require('fs');
 const app = express ()
 const port = 5000
 
+const data = JSON.parse(fs.readFileSync('project-data.json', 'utf-8'));
+
 app.use(express.static('public'))
 app.use('./css', express.static(__dirname + 'public/css'))
 app.use('./js', express.static(__dirname + 'public/js'))
@@ -13,9 +15,13 @@ app.use(expressLayouts)
 app.set('layout')
 app.set('view engine', 'ejs')
 
+app.use((req, res, next) => {
+    res.locals.projects = data;  // 'projects' will be available in all EJS templates
+    next();
+});
+
 app.get('/', (req, res) => {
-    const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
-    res.render('index', { projects: data });
+    res.render('index');
 });
 
 app.get('/about',(req, res) => {
@@ -27,19 +33,15 @@ app.get('/contact',(req, res) => {
 })
 
 app.get('/projects', (req, res) => {
-    fs.readFile('data.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).send('An error occurred');
-        }
+    res.render('projects');
+});
 
-        const projects = JSON.parse(data);
-        res.render('projects', { projects });
-    });
+app.get('/artpage', (req, res) => {
+    const artworkData = JSON.parse(fs.readFileSync('art-data.json', 'utf-8'));
+    res.render('artpage', { artworks: artworkData });
 });
 
 app.get('/:page', (req, res) => {
-    const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
     const pageData = data.find(d => d.url === req.params.page);
     if (pageData) {
         res.render('template', pageData);
